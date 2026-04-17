d9042988e3a02ca9b0a5a6ce23399381e667eb6f
Farshid Mousavi <r.shidef@gmail.com>
2026-04-17 21:44:45 +0330

    remove second from file name

--- internal/backup/backup.go
+++ internal/backup/backup.go
@@ -11,38 +11,37 @@ import (
 	"github.com/farshidmousavii/netmon/internal/logger"
 )
 
-
-func WriteToFile(hostname, deviceType, output, backupDirectory, archivePath string) (string , error) {
-	now := time.Now().Format("2006-01-02_15-04-05")
+func WriteToFile(hostname, deviceType, output, backupDirectory, archivePath string) (string, error) {
+	now := time.Now().Format("2006-01-02_15-04")
 
 	dirPath := filepath.Join(backupDirectory, deviceType, now)
 
 	if err := os.MkdirAll(dirPath, 0755); err != nil {
-		return "",fmt.Errorf("create backup directory: %w", err)
+		return "", fmt.Errorf("create backup directory: %w", err)
 	}
 
 	ext, err := deviceExtension(deviceType)
 	if err != nil {
-		return "",err
+		return "", err
 	}
 
 	hostname = filepath.Base(hostname)
 	fileName := fmt.Sprintf("%s.%s", hostname, ext)
 	filePath := filepath.Join(dirPath, fileName)
 
-	logger.Info("Start backup from %s" , hostname)
+	logger.Info("Start backup from %s", hostname)
 	// atomic write
 	if err := atomicWrite(filePath, []byte(output)); err != nil {
-		return "",fmt.Errorf("write config file: %w", err)
+		return "", fmt.Errorf("write config file: %w", err)
 	}
 
 	if archivePath != "" {
 		if err := moveFile(filePath, archivePath); err != nil {
-			return "",fmt.Errorf("move to archive: %w", err)
+			return "", fmt.Errorf("move to archive: %w", err)
 		}
 	}
 
-	return filePath,nil
+	return filePath, nil
 }
 
 func deviceExtension(deviceType string) (string, error) {
